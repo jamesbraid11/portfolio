58e6f36f7c90e87e0047113d543ab3ba1e9d7205
jamesbraid <james_braid@hotmail.co.uk>
2024-02-23 20:48:19 +0000

    Basic about section complete.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,22 @@
 
 export default function About() {
   return (
-    <section id="about">
+    <section id="about-screen">
       <h1>About</h1>
+      <div className="about-container">
+        <div className="about about-image"></div>
+        <div className="about about-text">
+          <p className="about-p">
+            In my career to date, I've navigated diverse industries, honing my skills in problem-solving and creative thinking. My journey, from optimising ATM maintenance at RBS to shaping students' growth as a primary school teacher, has fueled my passion for development. Now fully immersed in my career as a developer, I bring a growth mindset and leadership experience.
+          </p>
+          <p className="about-p">
+            My commitment to excellence extends beyond coding – I've excelled in communication, organisational prowess, and professional development. As a former teacher, I bring a reflective and dynamic work ethic, consistently delivering innovative solutions. A former Growth Mindset Champion, Maths Lead, and leader in cross-academy analyses, my impact resonates through fostering resilience and passion for learning.
+          </p>
+          <p className="about-p">
+            With a keen eye for efficiency and an engaging approach, I thrive in collaborative environments. I am not just a developer; I am a dedicated professional who brings a wealth of skills and a passion for impactful, creative, and efficient work.
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
