da0931d7d5e9adedd473c78208d87c9534482eda
jamesbraid <james_braid@hotmail.co.uk>
2024-02-28 10:07:20 +0000

    Text edit on hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section id="hero-screen">
       {/* <div className="title-container"> */}
-        <h1>James Braid<br></br>Junior Web Developer</h1>
+        <h1>James Braid<br></br>Junior Software Engineer</h1>
         <TextAnimation />
       {/* </div> */}
     </section>
