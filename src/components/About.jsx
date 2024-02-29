bf3460c7ec020b896a56e076cacc663ab13523cd
jamesbraid <james_braid@hotmail.co.uk>
2024-02-29 10:17:42 +0000

    About text update to include interests

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,10 @@ export default function About() {
             In my career to date, I've navigated diverse industries, honing my skills in problem-solving and creative thinking. My journey, from optimising ATM maintenance at RBS to shaping students' growth as a primary school teacher, has fueled my passion for development. Now fully immersed in my career as a developer, I bring a growth mindset and leadership experience.
           </p>
           <p className="about-p">
-            My commitment to excellence extends beyond coding – I've excelled in communication, organisational prowess, and professional development. As a former teacher, I bring a reflective and dynamic work ethic, consistently delivering innovative solutions. A former Growth Mindset Champion, Maths Lead, and leader in cross-academy analyses, my impact resonates through fostering resilience and passion for learning.
+            My commitment to excellence extends beyond coding – I've excelled in communication, organisational prowess, and professional development. As a former teacher, I bring a reflective and dynamic work ethic, consistently delivering innovative solutions. With a keen eye for efficiency and an engaging approach, I thrive in collaborative environments. I am not just a developer; I am a dedicated professional who brings a wealth of skills and a passion for impactful and creative work.
           </p>
           <p className="about-p">
-            With a keen eye for efficiency and an engaging approach, I thrive in collaborative environments. I am not just a developer; I am a dedicated professional who brings a wealth of skills and a passion for impactful and creative work.
+            Outside of my professional life, I enjoy DJing and fine dining. I also love all things football: I have had a Crystal Palace season ticket for many years now, and I enjoy playing online fantasy football with friends.
           </p>
         </div>
       </div>
